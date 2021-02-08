5f32f9bef2393ac4b7213258718fa6487166abc9
cgomillion <cgomillion2@gmail.com>
2021-02-08 18:56:20 -0500

    psuedo coding for the game logic, trying to get my structure set

--- js/app.js
+++ js/app.js
@@ -1,15 +1,38 @@
 console.log('Testing console')
 
+// need variables for the level, an array for the userInput and simonInput
+let userLevel = $('.count')
+let userInput = []
+let simonInput = []
+
+
+$( () =>  {
+
+
 class Level {
     constructor(){
-
+        this.userLevel = 1
     }
+    
+    // needs to start on the click of the Start button, => .startButton
+    $('.startButton').click( () => {
+        
+
+    })
 }
+
+// need a function to turn the colors grey, then turn back
+
+// need a function to get random (Simons) colors, increments with level to start.
+
+// need a function to log the users input 
+
+// check it to Simons if wrong game over
 
 class Game {
     constructor() {
-
     }
+    
 }
 
 class NewGame {
@@ -17,3 +40,5 @@ class NewGame {
         
     }
 }
+
+})
