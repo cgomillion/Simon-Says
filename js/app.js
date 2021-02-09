bb5f09500f97ae1cacbc21f69ea945b0dd477bd3
cgomillion <cgomillion2@gmail.com>
2021-02-08 20:53:35 -0500

    Start button logs levels, simons moves still don't work yet

--- js/app.js
+++ js/app.js
@@ -1,37 +1,41 @@
 console.log('Testing console')
 
 // need variables for the level, an array for the userInput and simonInput
-let userLevel = $('.count')
+let userLevel = 0
 let userInput = []
 let simonInput = []
 let colors = [$('.blue'), $('.green'), $('.red'), $('.yellow')]
 
-$( () =>  { // goint to try and use jquery
+ // goint to try and use jquery
     // need a function to get random (Simons) colors, increments with level to start.
+    $(".startButton").click( () => {
+    console.log('clicked registered') 
     userLevel++
+    $('.count').text(`Level : ${userLevel}`)
     for (let i = 0; i <= userLevel; i++) {
     let randomNum = Math.floor(Math.random * 4)
-        let simonColor = colors[randomNum]
+        let simonColor = []
+        simonColor.push(colors[randomNum])
 
         // need a function to turn the colors grey, then turn back
         
-        let logSimon() = function() {
-            $(simonColor).addClass('active')
+        let logSimon = () => {
+            $(simonColor[i]).addClass('.active')
             setTimeout(() => {
 
             },500)
-            $(simonColor).removeClass('active')
+            $(simonColor[i]).removeClass('.active')
         }
         // need a function to log the users input
-        let logUser
+        logSimon()
     }   
-
+})
     // needs to start on the click of the Start button, => .startButton
-    $(".startButton").click( () => {
-        console.log('clicked registered') 
+    // $(".startButton").click( () => {
+    //     console.log('clicked registered') 
+    //     logSimon()
         
-        
-    })
+    // })
 
 
 
@@ -43,4 +47,3 @@ $( () =>  { // goint to try and use jquery
 
 
 
-})
