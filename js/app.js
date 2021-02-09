704c05714c9ddcd648bcc4f6c3ecb7f16c4e6f8d
cgomillion <cgomillion2@gmail.com>
2021-02-08 19:34:32 -0500

    restructured the app.js, think I'm on a better track

--- js/app.js
+++ js/app.js
@@ -4,41 +4,43 @@ console.log('Testing console')
 let userLevel = $('.count')
 let userInput = []
 let simonInput = []
+let colors = [$('.blue'), $('.green'), $('.red'), $('.yellow')]
 
+$( () =>  { // goint to try and use jquery
+    // need a function to get random (Simons) colors, increments with level to start.
+    userLevel++
+    for (let i = 0; i <= userLevel; i++) {
+    let randomNum = Math.floor(Math.random * 4)
+        let simonColor = colors[randomNum]
 
-$( () =>  {
-
-
-class Level {
-    constructor(){
-        this.userLevel = 1
-    }
-    
-    // needs to start on the click of the Start button, => .startButton
-    $('.startButton').click( () => {
+        // need a function to turn the colors grey, then turn back
         
+        let logSimon() = function() {
+            $(simonColor).addClass('active')
+            setTimeout(() => {
 
+            },500)
+            $(simonColor).removeClass('active')
+        }
+        // need a function to log the users input
+        let logUser
+    }   
+
+    // needs to start on the click of the Start button, => .startButton
+    $(".startButton").click( () => {
+        console.log('clicked registered') 
+        
+        
     })
-}
 
-// need a function to turn the colors grey, then turn back
 
-// need a function to get random (Simons) colors, increments with level to start.
 
-// need a function to log the users input 
+
+
+ 
 
 // check it to Simons if wrong game over
 
-class Game {
-    constructor() {
-    }
-    
-}
 
-class NewGame {
-    constructor() {
-        
-    }
-}
 
 })
