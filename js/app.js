9b808e27d5c34dfb11a54096225371663687281c
cgomillion <cgomillion2@gmail.com>
2021-02-09 20:18:45 -0500

    simons move is interacting, and i have randomized simons moves

--- js/app.js
+++ js/app.js
@@ -1,41 +1,203 @@
 console.log('Testing console')
 
-// need variables for the level, an array for the userInput and simonInput
-let userLevel = 0
-let userInput = []
-let simonInput = []
-let colors = [$('.blue'), $('.green'), $('.red'), $('.yellow')]
 
- // goint to try and use jquery
-    // need a function to get random (Simons) colors, increments with level to start.
-    $(".startButton").click( () => {
-    console.log('clicked registered') 
-    userLevel++
-    $('.count').text(`Level : ${userLevel}`)
-    // for (let i = 0; i <= userLevel; i++) {
-    let randomNum = Math.floor(Math.random * 4)
-        let simonColor = []
-        simonColor.push(colors[randomNum])
+// reference video : 
+// Simon Game JavaScript Tutorial for Beginners
+// https://www.youtube.com/watch?v=n_ec3eowFLQ&t=147s
+// need variables for my moves, simons moves, the flashing of the panels, boolean if it is correct, boolean if you are a winner, and setting a variable to the interval.
+let userLevel = 0;
+let userInput = [];
+let simonInput = [];
+let flashAction;
+let correct;
+let simonTurn;
+let winner;
+let interval;
+let on = true;
 
-        // need a function to turn the colors grey, then turn back
-        
-        let logSimon = () => {
-            $(simonColor).addClass('.active')
-            setTimeout(() => {
+const $levelCounter = $('.count');
+const $topLeftBlue = $('.top-left-blue');
+const $topRightGreen = $('.top-right-green');
+const $bottomLeftRed = $('bottom-left-red');
+const $bottomRightYellow = $('.bottom-right-yellow');
+const $startButton = $('.startButton');
 
-            },500)
-            $(simonColor).removeClass('.active')
-        }
-        // need a function to log the users input
-        logSimon()
-    // }   
+$(() => {
+function playGame() {
+    winner = false;
+    simonInput = []; 
+    userInput = [];
+    flashAction = 0;
+    interval = 0;
+    userLevel = 1;
+    $levelCounter.text(`Level : ${userLevel}`);
+    console.log(userLevel);
+    correct = true;
+
+    for (let i = 0; i < 20; i++) {
+        simonInput.push(Math.floor(Math.random() * 4) + 1);
+    }
+    console.log(simonInput)
+    simonTurn = true;
+    interval = setInterval(gameLevel, 800);
+}
+
+function gameLevel() {
+    on = false; 
+
+    if (flashAction === userLevel) {
+        clearInterval(interval);
+        simonTurn = false;
+        clearBoard();
+        on = true;
+    }
+
+    if (simonTurn === true) {
+        clearBoard();
+        setTimeout(() => {
+            if (simonInput[flashAction] === 1) oneBlue();
+            if (simonInput[flashAction] === 2) twoGreen();
+            if (simonInput[flashAction] === 3) threeRed();
+            if (simonInput[flashAction] === 4) fourYellow();
+            flashAction++;
+        }, 300);
+    }
+}
+
+function oneBlue() {
+    $topLeftBlue.css('background-color', 'lightblue')
+}
+
+function twoGreen() {
+    $topRightGreen.css('background-color', 'lightgreen')
+}
+
+function threeRed() {
+    $bottomLeftRed.css('background-color', 'tomato')
+}
+
+function fourYellow() {
+    $bottomRightYellow.css('background-color', 'yellow')
+}
+
+function clearBoard() {
+    $bottomRightYellow.css('background-color', 'goldenrod');
+    $bottomLeftRed.css('background-color', 'darkred');
+    $topRightGreen.css('background-color', 'darkgreen');
+    $topLeftBlue.css('background-color', 'darkblue');
+}
+
+$($topLeftBlue).click(() => {
+    if (on) {
+      userInput.push(1);
+    //   check();
+      oneBlue();
+      if(!winner) {
+        setTimeout(() => {
+          clearBoard();
+        }, 300);
+      }
+    }
+  })
+
+$($topRightGreen).click(() => {
+    if (on) {
+      userInput.push(2);
+    //   check();
+      twoGreen();
+      if(!winner) {
+        setTimeout(() => {
+          clearBoard();
+        }, 300);
+      }
+    }
+  })
+
+$($bottomLeftRed).click(() => {
+    if (on) {
+      userInput.push(3);
+    //   check();
+      threeRed();
+      if(!winner) {
+        setTimeout(() => {
+          clearBoard();
+        }, 300);
+      }
+    }
+  })
+
+
+$($bottomRightYellow).click(() => {
+    if (on) {
+      userInput.push(4);
+    //   check();
+      fourYellow();
+      if(!winner) {
+        setTimeout(() => {
+          clearBoard();
+        }, 300);
+      }
+    }
+  })
+
+
+$($startButton).click(() => {
+    if (on || winner) {
+    playGame();
+    }
 })
-    // needs to start on the click of the Start button, => .startButton
-    // $(".startButton").click( () => {
-    //     console.log('clicked registered') 
-    //     logSimon()
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  // goint to try and use jquery
+//     // need a function to get random (Simons) colors, increments with level to start.
+//     $(".startButton").click( () => {
+//     console.log('clicked registered') 
+//     userLevel++
+//     $('.count').text(`Level : ${userLevel}`)
+//     // for (let i = 0; i <= userLevel; i++) {
+//     let randomNum = Math.floor(Math.random * 4)
+//         let simonColor = []
+//         simonColor.push(colors[randomNum])
+
+//         // need a function to turn the colors grey, then turn back
         
-    // })
+//         let logSimon = () => {
+//             $(simonColor).addClass('.active')
+//             setTimeout(() => {
+
+//             },500)
+//             $(simonColor).removeClass('.active')
+//         }
+//         // need a function to log the users input
+//         logSimon()
+//     // }   
+// })
+//     // needs to start on the click of the Start button, => .startButton
+//     // $(".startButton").click( () => {
+//     //     console.log('clicked registered') 
+//     //     logSimon()
+        
+//     // })
 
 
 
@@ -43,7 +205,7 @@ let colors = [$('.blue'), $('.green'), $('.red'), $('.yellow')]
 
  
 
-// check it to Simons if wrong game over
+// // check it to Simons if wrong game over
 
 
 
